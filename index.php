930d3887d70d239c6f33267d42db8d1757a799d1
Santhan Ratakonda <49435109+SanthanRatakonda@users.noreply.github.com>
2023-04-19 12:17:33 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Santhan Ratakonda | Portfolio</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Santhan Ratakonda</h1>
+		<nav>
+			<ul>
+				<li><a href="#education">Education</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li><a href="#experience">Professional Experience</a></li>
+				<li><a href="#projects">Projects</a></li>
+				<li><a href="#certifications">Certifications</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<main>
+		<section id="education">
+			<h2>Education</h2>
+			<ul>
+				<li>Master of Science, Computer Science (GPA : 3.83), University of Central Florida, 01/2022 – 12/2023</li>
+				<li>Bachelor of Technology, Computer Science and Engineering, Kalasalingam Academy of Research and Education, 07/2017 – 07/2021</li>
+			</ul>
+		</section>
+		
+		<section id="skills">
+			<h2>Skills</h2>
+			<ul>
+				<li>Programming Languages: Java, Python, C , C++ , Swift, R</li>
+				<li>Tools: Git, Power BI, GitHub, Anaconda</li>
+				<li>Database: MySQL, PostgreSQL, MongoDB</li>
+				<li>Scripting Languages: HTML, CSS, JavaScript, ReactJS</li>
+				<li>Cloud Technologies: AWS, Microsoft Azure</li>
+				<li>Data Processing & Integration: Pandas, NumPy, ETL, scikit-learn</li>
+			</ul>
+		</section>
+		
+		<section id="experience">
+			<h2>Professional Experience</h2>
+			<ul>
+				<li>Graduate Teaching Assistant, University of Central Florida, 01/2022 – present</li>
+				<li>Software Engineering Intern, Triculin Technologies, 06/2020 – 07/2020</li>
+			</ul>
+		</section>
+		
+		<section id="projects">
+			<h2>Projects</h2>
+			<ul>
+				<li>Car Insurance Purchase Prediction using Cross Sell Prediction, (Python)</li>
+				<li>Customer Churn Analysis for a Telecommunications Company, (Python)</li>
+				<li>Bandwidth EfficientImage Sharing during Disasters, (Java, Swing)</li>
+				<li>Home Automation using NodeMCU, (Java)</li>
+			</ul>
+		</section>
+		
+		<section id="certifications">
+			<h2>Certifications</h2>
+			<ul>
+				<li>AWS Developer Associate</li>
+				<li>SAP Certified Development Associate - ABAP with SAP NetWeaver 7.5</li>
+			</ul>
+		</section>
+	</main>
+	
+	<footer>
+		<p>Contact me:</p>
+		<ul>
+			<li>Email: santhanratakonda@gmail.com</li>
+			<li>Phone: 7043880528
